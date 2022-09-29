0dc026d49f1588185e9daf144073e0384ae032e4
Vlada <vladasilaeva@yandex.ru>
2022-09-30 00:11:04 +0300

    lab1_task7 time.h

--- lab1/lab1_task7.c
+++ lab1/lab1_task7.c
@@ -2,25 +2,30 @@
 и минимального элементов, поменять местами максимальный и минимальный элементы массива.*/
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 20
+#include <time.h>
+#define SIZE 10
 
-void fill_arr_rand(int N, int **arr, int min, int max){
+void fill_arr_rand(int N, int *arr, int min, int max){
     for (int i=0; i<N; i++){
-        (*arr)[i] = min + rand()%(max - min + 1);
+        arr[i] = min + rand()%(max - min + 1);
     }
 }
 
 int main(){
+    srand(time(NULL));
     int arr[SIZE], i_min=0, i_max=0;
-    fill_arr_rand(SIZE, &arr, 0, RAND_MAX);
+    fill_arr_rand(SIZE, arr, 0, RAND_MAX);
     for (int i=0; i<SIZE; i++){
-        printf("%d ", arr[i]);
+        printf("%d\t", arr[i]);
         if (arr[i]>arr[i_max]) i_max=i;
         if (arr[i]<arr[i_min]) i_min=i;
     }
+    printf("\n");
+    printf("Поменяем %d-тый(min=%d) и %d-тый(max=%d)\n", i_min, arr[i_min], i_max, arr[i_max]);
     arr[i_min] = arr[i_min] + arr[i_max];
     arr[i_max] = arr[i_min] - arr[i_max];
     arr[i_min] = arr[i_min] - arr[i_max];
-    for (int i=0; i<SIZE; i++) printf("%d ", arr[i]);
+    for (int i=0; i<SIZE; i++) printf("%d\t", arr[i]);
+    printf("\n");
     return 0;
 }
