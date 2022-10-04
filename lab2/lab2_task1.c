b911ef3c09ea38968eb784998cc457e9b9ea2339
Vlada <vladasilaeva@yandex.ru>
2022-10-04 14:22:50 +0300

    lab2_task1 print_arr (+ delete .exe)

--- lab2/lab2_task1.c
+++ lab2/lab2_task1.c
@@ -3,33 +3,39 @@
 #include <time.h>
 
 void fill_arr_rand(int N, int **arr, int min, int max){
+    if(N<0) return;
+    int*p = (int*)realloc((*arr), N*sizeof(int));
+    if (!p) return;
+    (*arr) = p;
     for (int i=0; i<N; i++){
         (*arr)[i] = min + rand()%(max - min + 1);
     }
 }
 
-void print_arr(int N, int *arr){
-    for (int i=0; i<N; i++) printf("%d\t", arr[i]);
-    printf("\n");
+void print_arr(int N, int *arr, const char* sep, const char* end){
+    if (!arr) {
+        printf("NULL%s", end);
+        return;
+    }
+    for (int i=0; i<N; i++) printf("%d%s", arr[i], sep);
+    printf("%s", end);
 }
 
 int main(){
     srand(time(NULL));
-    int *A, *B, *C, N, M;
-    scanf("%d", N);
-    scanf("%d", M);
-    A = (int*)malloc(N*sizeof(int));
+    int *A=NULL, *B=NULL, *C=NULL, N, M;
+    printf("Введитете размер массивов через пробел\n");
+    scanf("%d %d", &N, &M);
     C = (int*)malloc(N*sizeof(int));
-    B = (int*)malloc(M*sizeof(int));
     fill_arr_rand(N, &A, 0, RAND_MAX);
     fill_arr_rand(M, &B, 0, RAND_MAX);
     for (int i=0; i<N; i++){
         if (i>M-1) C[i] = A[i]+B[M-1];
         else C[i] = A[i]+B[i];
     }
-    print_arr(N, A);
-    print_arr(M, B);
-    print_arr(N, C);
+    printf("A:"); print_arr(N, A, "\t", "\n");
+    printf("B:"); print_arr(M, B, "\t", "\n");
+    printf("C:"); print_arr(N, C, "\t", "\n");
     free(A); 
     free(B); 
     free(C);
