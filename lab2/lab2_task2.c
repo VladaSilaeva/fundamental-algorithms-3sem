526f9293efb3522e5d854253b64c3bba535a6d96
Vlada <vladasilaeva@yandex.ru>
2022-10-07 11:15:32 +0300

    lab2_task2 realloc

--- lab2/lab2_task2.c
+++ lab2/lab2_task2.c
@@ -23,16 +23,7 @@ int len(const char *str) {
     return n;
 }
 
-char* reverse(const char *str, char **buf, int b_size){
-    char *p = *buf+b_size-1;
-    *(p--) = 0;
-    for(int i=0; (str[i]&&(i<b_size)); i++){
-        *(p--) = str[i];
-    }
-    return p;
-}
-
-void reverse2(const char *str, char **buf, int b_size){
+void reverse(const char *str, char **buf){
     int N=len(str);
     char *p = (char*)realloc(*buf, (N+1)*sizeof(char));
     if (!p) return;
@@ -42,37 +33,45 @@ void reverse2(const char *str, char **buf, int b_size){
     *buf = p;
 }
 
-void toupperodds(const char *str, char **buf, int b_size){
+void toupperodds(const char *str, char **buf){
+    int N=len(str);
+    char *p = (char*)realloc(*buf, (N+1)*sizeof(char));
+    if (!p) return;
+    *buf = p;
     int i=0;
-    for(; (str[i]&&(i<b_size-1)); i++){
+    for(; str[i]; i++){
         (*buf)[i] = i%2?toupper(str[i]):str[i];
     }
     (*buf)[i+1]=0;
 }
 
-void flag_n(const char *str, char **buf, int b_size){
-    int n=0, l=0, o=0;
-    char *letters, *others, *p=*buf;
-    letters = (char*)malloc(b_size*sizeof(char));
-    others = (char*)malloc(b_size*sizeof(char));
-    for(int i=0; (str[i]&&(i<b_size)); i++){
-        if (isdigit(str[i])){
-            *(p++) = str[i];
-            n++;
-        } else if(isalpha(str[i])) letters[l++]=str[i];
+void flag_n(const char *str, char **buf){
+    int N=len(str), l=0, o=0;
+    char *letters, *others, *p;
+    p = (char*)realloc(*buf, (N+1)*sizeof(char));
+    if (!p) return;
+    *buf = p;
+    letters = (char*)malloc(N*sizeof(char));
+    others = (char*)malloc(N*sizeof(char));
+    for(int i=0; str[i]; i++){
+        if (isdigit(str[i])) *(p++) = str[i];
+        else if(isalpha(str[i])) letters[l++]=str[i];
         else others[o++]=str[i];
     }
-    for(int i=0; (i<l)&&(n+i<b_size); i++) *(p++) = letters[i];
-    for(int i=0; (i<o)&&(n+l+i<b_size); i++) *(p++) = others[i];
+    for(int i=0; i<l; i++) *(p++) = letters[i];
+    for(int i=0; i<o; i++) *(p++) = others[i];
     p=0;
 }
 
-void concatenation(const char *str1, const char *str2, char **buf, int b_size){
-    char *p = *buf;
-    for(int i=0; (str1[i]&&(i<b_size)); i++){
+void concatenation(const char *str1, const char *str2, char **buf){
+    int N=len(str1)+len(str2);
+    char *p = (char*)realloc(*buf, (N+1)*sizeof(char));
+    if (!p) return;
+    *buf = p;
+    for(int i=0; str1[i]; i++){
         *(p++) = str1[i];
     }
-    for(int i=0; (str2[i]&&(i<b_size)); i++){
+    for(int i=0; str2[i]; i++){
         *(p++) = str2[i];
     }
     *p=0;
@@ -81,33 +80,39 @@ void concatenation(const char *str1, const char *str2, char **buf, int b_size){
 int main(int argc, char **argv)
 {
     char *buf=NULL;
-    int SIZE=100, ans;
-    if (argc > 2) SIZE=len(argv[2])+1;
-    buf = (char*)malloc(SIZE*sizeof(char)); //можно изначально выделить память
+    int ans, is_flags_correct=1;
     if (argc == 3) {
         if (strcmp(argv[1], "-l")==0 || strcmp(argv[1], "/l")==0) {//len
             ans=len(argv[2]);
             printf("Длина строки равна %d\n", ans);
         } else if (strcmp(argv[1], "-r")==0 || strcmp(argv[1], "/r")==0) {//reverse
-            //str = reverse(argv[2], &buf, SIZE);
-            reverse2(argv[2], &buf, SIZE);
+            reverse(argv[2], &buf);
             printf("Реверс строки %s\n", buf);
         } else if (strcmp(argv[1], "-u")==0 || strcmp(argv[1], "/u")==0) {//toupperodds
-            toupperodds(argv[2], &buf, SIZE);
+            toupperodds(argv[2], &buf);
             printf("Измененная строка %s\n", buf);
         } else if (strcmp(argv[1], "-n")==0 || strcmp(argv[1], "/n")==0) {//flag_n        
-            flag_n(argv[2], &buf, SIZE);
+            flag_n(argv[2], &buf);
             printf("Измененная строка %s\n", buf);
-        } else printf("Неправильный флаг или число аргументов\n");
+        } else {
+            is_flags_correct = 0;
+            printf("Неправильный флаг или число аргументов\n");
+        }
     }
     else if (argc == 4) {
         if (strcmp(argv[1], "-c")==0 || strcmp(argv[1], "/c")==0) {//concatenation
-            concatenation(argv[2], argv[3], &buf, SIZE);
+            concatenation(argv[2], argv[3], &buf);
             printf("Конкатенация строк %s\n", buf);
-        } else printf("Неправильный флаг или число аргументов\n");
+        } else {
+            is_flags_correct = 0;
+            printf("Неправильный флаг или число аргументов\n");
+        }
     }
-    else printf("Неправильное число аргументов\n");
-    //usage();
+    else {
+        is_flags_correct = 0;
+        printf("Неправильное число аргументов\n");
+    }
+    if (!is_flags_correct) usage();
     free(buf);
     return 0;
 }
