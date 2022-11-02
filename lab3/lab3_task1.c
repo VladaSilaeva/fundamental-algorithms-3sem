5f70c81c7f75276f8f18a47555ccebf4a95c4f99
Vlada <vladasilaeva@yandex.ru>
2022-11-02 23:20:27 +0300

    3.1 (fix incr, decr; work with negative nums)

--- lab3/lab3_task1.c
+++ lab3/lab3_task1.c
@@ -8,57 +8,91 @@
 #include <stdlib.h>
 #define OK 0
 #define INCORRECT_EXPONENT -1
-#define NOT_EMPTY_RES -2 
-#define MEMORY_ERROR -3
+#define TOO_SHORT_SIZE_OF_RES -2 
 
-int incr(int n) {
+int incr(int n) { //incr(2147483647)=-2147483648
     int i=1;
-    if (n == 1) return 2;
-    if (n < 0) {
-        //printf("%d\n", i);
-        while (!(n&i)) {
-            //printf("%d\n", i);
-            //printf("(%d&%d)=%d\n", n, i, (n&i));
-            n ^= i;
-            i <<= 1;
-        }
+    while (n&i){
         n ^= i;
-        return n;
+        i <<= 1;
     }
-    else {
-        while (n&i){
-            n ^= i;
-            i >>= 1;
-        }
-        n ^= i;
-    }
+    n ^= i;
     return n;
 }
 
-int dec_to_pow_of_two(int num, int r, char** res) {
-    int digit=0;
+int decr(int n) { //decr(-2147483648)=2147483647
+    int i=1;
+    while (!(n&i)&&(i)){
+        n ^= i;
+        i <<= 1;
+    }
+    n ^= i;
+    return n;
+}
+
+int dec_to_pow_of_two(int num, int r, char* res, int size) {
+    int mask=0, i=0, sign=0;
+    char digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+                     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+                     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
+                     'U', 'V', 'W', 'X', 'Y', 'Z'};
     if ((r<1)||(r>5)) return INCORRECT_EXPONENT;
-    if (*res) return NOT_EMPTY_RES;
-    *res=(char*)malloc(33*sizeof(char));
-    if (!*res) return MEMORY_ERROR;
-    
-    while (!((digit<<1)>>r)){
-        digit = 1 | (digit<<1);
+    while (!((mask<<1)>>r)){
+        mask = 1 | (mask<<1);
     }
-    int d = digit;
+    if (num<0) {
+        sign=1;
+        num=incr(~(num));
+    }
+    int n = num;
     while(num){
-        printf("%d (%d)", num&digit, num);
+        res[i] = digits[num&mask];
         num >>= r;
+        i = incr(i);
+        if (i==33) break;
     }
-    printf("\n");
+    if (!i) {res[0]='0'; res[1]=0; return OK;}
+    if ((i > size)||(sign&&(i==size))) return TOO_SHORT_SIZE_OF_RES;
+    if (sign) {
+        for (int j=i, k=0; k<j; j=decr(j), k=incr(k)){
+            res[j] ^= res[k];
+            res[k] ^= res[j];
+            res[j] ^= res[k];
+        }
+        res[0]='-';
+        res[incr(i)] = 0;
+    } else {
+        for (int j=decr(i), k=0; k<j; j=decr(j), k=incr(k)){
+            res[j] ^= res[k];
+            res[k] ^= res[j];
+            res[j] ^= res[k];
+        }
+        res[i] = 0;
+    }
     return OK;
 }
 
 
 int main() {
-    char* res[] = {NULL, NULL};
-    for (int i=-10; i<10; i++) printf("%d+1=%d\n", i, incr(i));
-    //dec_to_pow_of_two(10, 2, &res[0]);
-    //dec_to_pow_of_two(15, 2, &res[1]);
-    return 0;
+    int size=33, num1=31, num2=-31;
+    char res[size];
+    for (int i=0; i<33; i++) res[i]=0;
+    printf("%d\t\t(10)\n", num1);
+    for (int i=1; i<6; i=incr(i)) {
+        dec_to_pow_of_two(num1, i, res, size);
+        printf("%s\t\t(%d)\n", res, 1<<i);
+    }
+    printf("\n%d\t\t(10)\n", num2);
+    for (int i=1; i<6; i=incr(i)) {
+        dec_to_pow_of_two(num2, i, res, size);
+        printf("%s\t\t(%d)\n", res, 1<<i);
+    }
+    printf("\n0\t\t(10)\n");
+    for (int i=1; i<6; i=incr(i)) {
+        dec_to_pow_of_two(0, i, res, size);
+        printf("%s\t\t(%d)\n", res, 1<<i);
+    }
 }
+/* A(10) B(11) C(12) D(13) E(14) F(15) G(16) H(17) I(18) J(19)
+   K(20) L(21) M(22) N(23) O(24) P(25) Q(26) R(27) S(28) T(29)
+   U(30) V(31) W(32) X(33) Y(34) Z(35)*/
